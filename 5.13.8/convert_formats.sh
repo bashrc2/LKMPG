ad566eb75c5da4b8bd55aa16669533b08876623a
Bob Mottram <bob@freedombone.net>
2021-09-09 21:29:57 +0100

    Add title

--- 5.13.8/convert_formats.sh
+++ 5.13.8/convert_formats.sh
@@ -5,5 +5,5 @@ pandoc -f html -t markdown -o LKMPG-${VERSION}.md LKMPG-${VERSION}.html
 pandoc -f markdown -t rst -o LKMPG-${VERSION}.rst LKMPG-${VERSION}.md
 git add LKMPG-${VERSION}.md
 git add LKMPG-${VERSION}.rst
-ebook-convert LKMPG-${VERSION}.html LKMPG-${VERSION}.epub --cover img/cover.png --authors "Peter Jay Salzman, Michael Burian, Ori Pomerantz, Bob Mottram" --language "English" --tags "kernel, programming, C, linux" --comments "Learn how to write modules for the Linux kernel"
+ebook-convert LKMPG-${VERSION}.html LKMPG-${VERSION}.epub --cover img/cover.png --authors "Peter Jay Salzman, Michael Burian, Ori Pomerantz, Bob Mottram" --language "English" --tags "kernel, programming, C, linux" --comments "Learn how to write modules for the Linux kernel" --title "The Linux Kernel Module Programmers Guide"
 cp LKMPG-${VERSION}.epub ../lkmpg.epub
